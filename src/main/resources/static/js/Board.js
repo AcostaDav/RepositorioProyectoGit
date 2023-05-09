d6ce911c9c15ddf88865891887db7a1d9676e68a
RubenAlcalde17 <131373201+RubenAlcalde17@users.noreply.github.com>
2023-05-09 17:34:31 +0000

    Contador

--- src/main/resources/static/js/Board.js
+++ src/main/resources/static/js/Board.js
@@ -190,3 +190,16 @@ class Board {
         }    	
     }
 }
+    var seconds = 0;
+    function secondPassed(){
+        var minutes =Math.round((seconds - 30)/60);
+        var remainingSeconds = seconds % 60;
+        if (remainingSeconds<10) {
+            remainingSeconds = "0" + remainingSeconds;
+        }
+        document.getElementById('countdown').innerHTML=minutes+":"+remainingSeconds;
+        seconds++;
+        var countFinish = setTimeout(reset,120000);
+    } 
+    var countdownTimer = setInterval(secondPassed,1000);
+
